81966a5b20a9ac2d418c4925bcec0bd5134b3fc7
blackars <castillo.brayan@correounivalle.edu.co>
2025-09-03 17:18:19 -0500

    ajustando experiencia nbases para manejar three

--- experiences/n-bases/nbases.js
+++ experiences/n-bases/nbases.js
@@ -1,6 +1,6 @@
-import * as THREE from "/node_modules/three/build/three.module.js";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from "/assets/js/loaders/GLTFLoader.js";
+import * as THREE from "/assets/js/three.module.js";
+import { OrbitControls } from '/assets/js/OrbitControls.js';
+import { GLTFLoader } from "/assets/js/GLTFLoader.js";
 
 // ============================
 // Setup básico

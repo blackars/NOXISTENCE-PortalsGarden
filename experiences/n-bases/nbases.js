0779c78ec1c76f3695ae07202066f24fe4f61dbc
blackars <castillo.brayan@correounivalle.edu.co>
2025-09-03 17:41:19 -0500

    ajustando importaciones de modulos three.js

--- experiences/n-bases/nbases.js
+++ experiences/n-bases/nbases.js
@@ -1,6 +1,6 @@
-import * as THREE from "/node_modules/three/build/three.module.js";
-import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from "/assets/js/loaders/GLTFLoader.js";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // ============================
 // Setup básico

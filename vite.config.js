86460f4a106eafc53d12bf27d5b2bb307b3472c6
blackars <castillo.brayan@correounivalle.edu.co>
2025-09-03 15:48:26 -0500

    corrigiendo rutas relativas en maxkodiaplanet.html

--- vite.config.js
+++ vite.config.js
@@ -22,8 +22,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         // Experiencias adicionales
         anothearth: resolve(__dirname, 'experiences/AnothEarth/anothearth.html'),
-        maxkodiaplanet: resolve(__dirname, 'experiences/maxkodiaplanet/maxkodiaplanet.html'),
         gameoflife: resolve(__dirname, 'experiences/game-life/gameoflife.html'),
+        maxkodiaplanet: resolve(__dirname, 'experiences/maxkodiaplanet/maxkodiaplanet.html'),
         cleansquaredplace: resolve(__dirname, 'experiences/clean-squaredplace/cleansquaredplace.html'),
         spacewords: resolve(__dirname, 'experiences/space-words/spacewords.html'),
         nbases: resolve(__dirname, 'experiences/n-bases/nbases.html'),
